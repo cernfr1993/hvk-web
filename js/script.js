73e10a44c5d0c6568651fe83fa10f5d27d67d7fb
Ubuntu <ubuntu@ip-172-31-31-36.us-west-2.compute.internal>
2013-07-16 15:41:56 +0000

    Added files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   $(".img").mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+    $(".img").mouseleave(function() { 
+	$(this).animate({
+	    height: '-=10px'
+	});
+    });
+});
